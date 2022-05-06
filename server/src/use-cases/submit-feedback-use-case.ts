98368fadda04e90be05b0d19c63796b1b82bdb4b
Igor <igor.gds.00@gmail.com>
2022-05-06 19:57:40 -0300

    Aula - 3

---
+++ server/src/use-cases/submit-feedback-use-case.ts
@@ -0,0 +1,46 @@
+import { MailAdapter } from "../adapters/mail-adapter";
+import { FeedbacksRepository } from "../repositories/feedbacks-repository";
+
+interface SubmitFeedbackUseCaseRequest {
+  type: string;
+  comment: string;
+  screenshot?: string;
+}
+
+export class SubmitFeedbackUseCase {
+
+  constructor(
+    private feedbacksRepository: FeedbacksRepository,
+    private mailAdapter: MailAdapter
+  ) { }
+
+  async execute(req: SubmitFeedbackUseCaseRequest) {
+    const { type, comment, screenshot } = req;
+
+    if (!type) {
+      throw new Error('Type is required.');
+    }
+    if (!comment) {
+      throw new Error('Comment is required.');
+    }
+    if (screenshot && !screenshot.startsWith('data:image/png;base64')) {
+      throw new Error('Invalid screenshot format.')
+    }
+
+    this.feedbacksRepository.create({
+      type,
+      comment,
+      screenshot
+    });
+
+    await this.mailAdapter.sendMail({
+      subject: 'Novo feedback',
+      body: [
+        `<div style="fonst-family: sans-serif; fonst-size: 16px; color: #222;">`,
+        `<p>Tipo do feedback: ${type}</p>`,
+        `<p>Commentário: ${comment}</p>`,
+        `</div>`,
+      ].join('\n')
+    })
+  }
+}
